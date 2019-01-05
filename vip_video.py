cf959e8c77eed9759f16a8a5e57649282881e9d9
gaojian <gaojian_1010@126.com>
2019-01-05 16:42:21 +0800

    vip视频

---
+++ vip_video.py
@@ -0,0 +1,145 @@
+#!usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+# Author: Jan Gao
+# Date: 2019/1/4
+# Description: 腾讯视频、优酷、爱奇艺、pptv、乐视vip视频下载
+# Site: http://www.xrtpay.com/
+# Copyright (c) ShenZhen XinRuiTai Payment Service Co.,Ltd. All rights reserved
+"""
+import os
+import re
+import shutil
+import ssl
+import requests
+from lxml import etree
+from urllib.request import urlretrieve
+from multiprocessing import Pool
+ssl._create_default_https_context = ssl._create_unverified_context
+
+
+class DownloadVideo(object):
+    def __init__(self, url):
+        self.base_url = 'http://jx.618g.com/?url=' + url
+        self.header = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, '
+                                     'like Gecko) Chrome/71.0.3578.80 Safari/537.36'}
+        self.url = ''
+        self.thread_num = 32
+        self.p = 0
+        self.title = ''
+        self.ts_list = []
+
+    def get_page(self):
+        """
+        获取页面
+        :return:
+        """
+        try:
+            res = requests.get(self.base_url, headers=self.header)
+            if 200 == res.status_code:
+                print('获取目标网页成功， 准备下载ts...')
+                self.header['referer'] = self.base_url
+            return res.text
+        except Exception as e:
+            print('获取目标网页失败，请检查错误重试')
+            return e
+
+    def parse_page(self, html):
+        """
+        解析页面
+        :param html:
+        :return:
+        """
+        el = etree.HTML(html)
+        self.title = el.xpath('//title/text()')[0]
+        print(self.title)
+        m3u8_url = el.xpath('//div[@id="a1"]/iframe/@src')[0].split('url=')[1]
+        print(m3u8_url)  # https://tudou.com-l-tudou.com/20181226/15303_6239bcf5/index.m3u8
+        m3u8_text = self.get_m3u8_1(m3u8_url).strip()   # 1000k/hls/index.m3u8
+        self.url = m3u8_url[:-10] + m3u8_text   # https://tudou.com-l-tudou.com/20181226/15303_6239bcf5/1000k/hls/index.m3u8
+        self.ts_list = self.get_m3u8_2(self.url)  # 'd8c37751d3e000000.ts'
+
+    def get_m3u8_1(self, url):
+        """
+
+        :param url:
+        :return:
+        """
+        try:
+            resp = requests.get(url, headers=self.header)
+            print('获取ts文件成功，准备提取信息...')
+            print(resp.text[-20:])
+            return resp.text[-20:]  # 1000k/hls/index.m3u8
+        except Exception as e:
+            print('缓存文件请求错误1，请检查错误')
+            print(e)
+
+    def get_m3u8_2(self, url):
+        """
+        获取ts
+        :param url:
+        :return:
+        """
+        response = requests.get(url, headers=self.header)
+        print(response.text)
+        return self.parse_ts(response.text)
+
+    def parse_ts(self, text):
+        """
+        解析匹配ts
+        :param text:
+        :return:
+        """
+        pattern = re.compile('.+ts')
+        self.ts_list = re.findall(pattern, text)
+        print('提取信息完成，准备下载...')
+        self.pool()
+
+    def save_ts(self, ts):
+        """
+        保存ts文件
+        :return:
+        """
+        try:
+            ts_url = self.url[:-10] + ts
+            self.p += 1
+            a = urlretrieve(ts_url, self.title + '/{}'.format(ts))
+            print(a[0])
+        except Exception as e:
+            print('下载ts文件错误，请检查重试')
+            print(e)
+
+    def pool(self):
+        print('需要下载的文件有：%s' % len(self.ts_list))
+        # if self.title not in os.listdir(os.path.dirname(__file__)):
+        #     os.mkdir(self.title)
+        # pool = Pool(16)
+        # pool.map(self.save_ts, [ts for ts in self.ts_list])
+        # pool.close()
+        # pool.join()
+        print('下载完成...')
+
+        self.ts_to_mp4()
+
+    def ts_to_mp4(self):
+        print('ts文件正在转录mp4...')
+        # cmd_str = 'cp -R ' + self.title.replace(' ', '/ ') + '*.ts ' + self.title.replace(' ', '/ ')+'.mp4'
+        cmd_str = 'ffmpeg -i /Users/gaojian/python_study/video/古董局中局第11集/*.ts /Users/gaojian/python_study/video/古董局中局第11集/ddd.mp4'
+        os.system('y')
+        print(cmd_str)
+        os.system(cmd_str)
+
+        mv_name = self.title+'.mp4'
+        if os.path.isfile(mv_name):
+            print('ts转换完成，祝你观影愉快！')
+            shutil.rmtree(self.title)
+
+
+if __name__ == '__main__':
+    v_url = 'https://v.qq.com/x/cover/9z88hz9qwugb8p1/s0029vmkpeh.html'
+    # v_url = 'https://v.qq.com/x/cover/yqlvu10akee76oy.html'
+    down = DownloadVideo(v_url)
+    h = down.get_page()
+    down.parse_page(h)
+
+
