156f4b5da50b8dfb020b95eef0ea1fd9a0def50d
gaojian <gaojian_1010@126.com>
2019-01-07 20:44:16 +0800

    修改

--- vip_video.py
+++ vip_video.py
@@ -11,6 +11,8 @@ import os
 import re
 import shutil
 import ssl
+import traceback
+
 import requests
 from lxml import etree
 from urllib.request import urlretrieve
@@ -20,7 +22,7 @@ ssl._create_default_https_context = ssl._create_unverified_context
 
 class DownloadVideo(object):
     def __init__(self, url):
-        self.base_url = 'http://jx.618g.com/?url=' + url
+        self.get_url = 'http://jx.618g.com/?url=' + url
         self.header = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, '
                                      'like Gecko) Chrome/71.0.3578.80 Safari/537.36'}
         self.url = ''
@@ -35,14 +37,14 @@ class DownloadVideo(object):
         :return:
         """
         try:
-            res = requests.get(self.base_url, headers=self.header)
+            res = requests.get(self.get_url, headers=self.header)
             if 200 == res.status_code:
                 print('获取目标网页成功， 准备下载ts...')
-                self.header['referer'] = self.base_url
+                self.header['referer'] = self.get_url
             return res.text
-        except Exception as e:
+        except Exception:
+            print(traceback.print_exc())
             print('获取目标网页失败，请检查错误重试')
-            return e
 
     def parse_page(self, html):
         """
@@ -51,22 +53,22 @@ class DownloadVideo(object):
         :return:
         """
         el = etree.HTML(html)
-        self.title = el.xpath('//title/text()')[0]
+        self.title = el.xpath('//title/text()')[0].replace(' ', '')
         print(self.title)
         m3u8_url = el.xpath('//div[@id="a1"]/iframe/@src')[0].split('url=')[1]
         print(m3u8_url)  # https://tudou.com-l-tudou.com/20181226/15303_6239bcf5/index.m3u8
         m3u8_text = self.get_m3u8_1(m3u8_url).strip()   # 1000k/hls/index.m3u8
-        self.url = m3u8_url[:-10] + m3u8_text   # https://tudou.com-l-tudou.com/20181226/15303_6239bcf5/1000k/hls/index.m3u8
-        self.ts_list = self.get_m3u8_2(self.url)  # 'd8c37751d3e000000.ts'
+        self.url = m3u8_url[:-10] + m3u8_text    # https://tudou.com-l-tudou.com/20181226/15303_6239bcf5/1000k/hls/index.m3u8
+        self.get_m3u8_2(self.url)
 
-    def get_m3u8_1(self, url):
+    def get_m3u8_1(self, m3_url):
         """
-
-        :param url:
+        获取m3u8
+        :param m3_url:
         :return:
         """
         try:
-            resp = requests.get(url, headers=self.header)
+            resp = requests.get(m3_url, headers=self.header)
             print('获取ts文件成功，准备提取信息...')
             print(resp.text[-20:])
             return resp.text[-20:]  # 1000k/hls/index.m3u8
@@ -81,7 +83,7 @@ class DownloadVideo(object):
         :return:
         """
         response = requests.get(url, headers=self.header)
-        print(response.text)
+        print(self.parse_ts(response.text))
         return self.parse_ts(response.text)
 
     def parse_ts(self, text):
@@ -111,22 +113,22 @@ class DownloadVideo(object):
 
     def pool(self):
         print('需要下载的文件有：%s' % len(self.ts_list))
-        # if self.title not in os.listdir(os.path.dirname(__file__)):
-        #     os.mkdir(self.title)
-        # pool = Pool(16)
-        # pool.map(self.save_ts, [ts for ts in self.ts_list])
-        # pool.close()
-        # pool.join()
+        if self.title not in os.listdir(os.path.dirname(__file__)):
+            os.mkdir(self.title)
+        pool = Pool(16)
+        pool.map(self.save_ts, [ts for ts in self.ts_list])
+        pool.close()
+        pool.join()
         print('下载完成...')
 
         self.ts_to_mp4()
 
     def ts_to_mp4(self):
         print('ts文件正在转录mp4...')
-        # cmd_str = 'cp -R ' + self.title.replace(' ', '/ ') + '*.ts ' + self.title.replace(' ', '/ ')+'.mp4'
-        cmd_str = 'ffmpeg -i /Users/gaojian/python_study/video/古董局中局第11集/*.ts /Users/gaojian/python_study/video/古董局中局第11集/ddd.mp4'
-        os.system('y')
-        print(cmd_str)
+        # win 系统
+        # cmd_str = 'copy /b C:\Users\Desktop\古董局中局第11集\* C:\Users\Desktop\古董局中局第11集\new.mp4'
+        # mac 或 linux
+        cmd_str = 'cat' + os.path.dirname(__file__) + self.title + '/*.ts > hello.mp4'
         os.system(cmd_str)
 
         mv_name = self.title+'.mp4'
